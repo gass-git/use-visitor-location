a1a74f80b7254d3ba548546abf6a97292d9a70cd
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2025-10-31 10:56:57 +0100

    fix: due to geolocation-db.com API blocks cross origin requests, the API has been changed to ipapi.co

--- src/hook.ts
+++ src/hook.ts
@@ -1,52 +1,78 @@
 import {useState, useEffect} from "react"
 import axios from "axios"
 
-/*
-example response
-
-{
-	"country_code":"HU",
-	"country_name":"Hungary",
-	"city":"Tapioszecso",
-	"postal":2251,
-	"latitude":47.45,
-	"longitude":19.6,
-	"IPv4":"89.134.11.248",
-	"state":"Pest megye"
-}
-*/
+type Nullable<T> = {
+  [K in keyof T]: T[K] | null;
+};
 
 interface Location {
-  country_code: string | null;
-  country_name: string | null;
-  city: string | null;
-  postal: number | null;
-  latitude: number | null;
-  longitud: number | null;
-  IPv4: string | null;
-  state: string | null;
+  ip: string;
+  network: string;
+  version: string;
+  city: string;
+  region: string;
+  region_code: string;
+  country: string;
+  country_name: string;
+  country_code: string;
+  country_code_iso3: string;
+  country_capital: string;
+  country_tld: string;
+  continent_code: string;
+  in_eu: boolean;
+  postal: string;
+  latitude: number;
+  longitude: number;
+  timezone: string;
+  utc_offset: string;
+  country_calling_code: string;
+  currency: string;
+  currency_name: string;
+  languages: string;
+  country_area: number;
+  country_population: number;
+  asn: string;
+  org: string;
 }
 
-export default function useVisitorLocation(): Location {
+export default function useVisitorLocation(): Nullable<Location> {
   const [data, setData] = useState({
-    country_code: null,
-    country_name: null,
+    ip: null,
+    network: null,
+    version: null,
     city: null,
+    region: null,
+    region_code: null,
+    country: null,
+    country_name: null,
+    country_code: null,
+    country_code_iso3: null,
+    country_capital: null,
+    country_tld: null,
+    continent_code: null,
+    in_eu: null,
     postal: null,
     latitude: null,
-    longitud: null,
-    IPv4: null,
-    state: null
+    longitude: null,
+    timezone: null,
+    utc_offset: null,
+    country_calling_code: null,
+    currency: null,
+    currency_name: null,
+    languages: null,
+    country_area: null,
+    country_population: null,
+    asn: null,
+    org: null,
   })
 
   // on-mount fetch
   useEffect(() => {
-    const fetch = async () => {
-      const JSONresp = await axios.get("https://geolocation-db.com/json")
-      const parsedToJs = JSON.parse(JSONresp.toString())
+    (async function fetch(){
+      const resp = await axios.get("https://ipapi.co/json")
 
-      setData(parsedToJs)
-    }
+      setData(resp.data)
+    })();
   }, []) // the hook has no dependencies
 
   return data
